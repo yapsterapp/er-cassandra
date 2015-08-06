9b0faaeb42054553be10e16bfc23f14d7cf7b880
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-06 16:20:45 +0100

    drift configuration

---
+++ src/er_cassandra/drift_migrate_config.clj
@@ -0,0 +1,14 @@
+(ns er-cassandra.drift-migrate-config
+  (:require
+   [plumbing.core :refer :all]
+   [er-cassandra.drift-migrations :as m]))
+
+(defnk cassandra
+  [alia-session keyspace namespace directory]
+
+  {:directory directory
+   :init (m/create-init-fn alia-session keyspace namespace)
+   :current-version m/current-version
+   :update-version m/update-version
+   :ns-content m/ns-content
+   :finished m/finished})
