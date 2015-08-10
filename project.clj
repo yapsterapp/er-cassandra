a1b51ba59c960517cb3f1329a07c2da9c4e6f3ac
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-10 16:42:28 +0100

    add potemkin and plumbing

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject employeerepublic/er-cassandra "0.2.1"
+(defproject employeerepublic/er-cassandra "0.2.7"
   :description "a simple cassandra conector"
   :url "https://github.com/employeerepublic/er-cassandra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.cli "0.3.2"]
+                 [potemkin "0.4.1"]
+                 [prismatic/plumbing "0.4.4"]
                  [mccraigmccraig/alia "2.7.2.1"]
                  [manifold "0.1.1-alpha3"]
-                 [drift "1.5.3"]]
+                 [drift "1.5.3"]
+                 [defurred "0.0.3"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [expectations "2.1.2"]]
