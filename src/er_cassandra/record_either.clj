22b9ff8be4fa8117e360fe309726a73e975153d6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-10 16:41:42 +0100

    Deferred[Either] responses for a monadic API

---
+++ src/er_cassandra/record_either.clj
@@ -0,0 +1,75 @@
+(ns er-cassandra.record-either
+  (:refer-clojure :exclude [update])
+  (:require
+   [potemkin :refer [import-vars]]
+   [cats.core :as m]
+   [cats.monad.either :as either]
+   [cats.monad.deferred :as dm]
+   [er-cassandra.record :as r]))
+
+(defn alia-error-transformer
+  "transforms an alia exception into an error map... doesn't
+   do much more than extract the ex-data map and set a message"
+  [x]
+  (let [data (if (instance? clojure.lang.ExceptionInfo x)
+               (ex-data x)
+               {:type (-> x type .getName keyword)})]
+    (merge {:message (.getMessage x)
+            :exception x}
+           data)))
+
+(import-vars [er-cassandra.record select-statement])
+
+(defn select
+  "select records : returns a Deferred[Either]"
+  ([session table key record-or-key-value]
+   (select session table key record-or-key-value {}))
+
+  ([session table key record-or-key-value opts]
+   (dm/either-deferred
+    (r/select session table key record-or-key-value opts))))
+
+(defn select-one
+  "select a single record : returns a Deferred[Either]"
+  ([session table key record-or-key-value]
+   (select-one session table key record-or-key-value {}))
+
+  ([session table key record-or-key-value opts]
+   (dm/either-deferred
+    (r/select-one session table key record-or-key-value opts))))
+
+(import-vars [er-cassandra.record insert-statement])
+
+(defn insert
+  "insert a single record - returns a Deferred[Either]"
+
+  ([session table record]
+   (insert session table record {}))
+
+  ([session table record opts]
+   (dm/either-deferred
+    (r/insert session table record opts))))
+
+(import-vars [er-cassandra.record update-statement])
+
+(defn update
+  "update a single record - returns a Deferred[Either]"
+
+  ([session table key record]
+   (update session table key record {}))
+
+  ([session table key record opts]
+   (dm/either-deferred
+    (r/update session table key record opts))))
+
+(import-vars [er-cassandra.record delete-statement])
+
+(defn delete
+  "delete a record - returns a Deferred[Either]"
+
+  ([session table key record-or-key-value]
+   (delete session table key record-or-key-value {}))
+
+  ([session table key record-or-key-value opts]
+   (dm/either-deferred
+    (r/delete session table key record-or-key-value opts))))
