94adb8ee12f854a3929e3678a086c0d91bc03429
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-11 16:24:37 +0100

    deferred and error handling in mock session

--- src/er_cassandra/session/mock.clj
+++ src/er_cassandra/session/mock.clj
@@ -1,12 +1,23 @@
 (ns er-cassandra.session.mock
+  (:require
+   [manifold.deferred :as d])
   (:import
    [er_cassandra.session Session]))
+
+(defn deferred-value
+  [v]
+  (let [d (d/deferred)]
+    (if (instance? Throwable v)
+      (d/error! d v)
+      (d/success! d v))
+    d))
 
 (defrecord MockSession [statement-responses]
   Session
   (execute [_ statement]
     (if (contains? statement-responses statement)
-      (get statement-responses statement)
+      (deferred-value
+        (get statement-responses statement))
 
       (throw (ex-info "no matching response" {:statement statement}))))
   (close [_]))
