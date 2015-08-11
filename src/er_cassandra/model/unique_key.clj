c7c558f1a4f9916a4c548a458a965898de6f5cf7
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-11 15:43:04 +0100

    updating record with key acquisition responses

--- src/er_cassandra/model/unique_key.clj
+++ src/er_cassandra/model/unique_key.clj
@@ -10,8 +10,7 @@
    [qbits.alia.manifold :as aliam]
    [qbits.hayt :as h]
    [er-cassandra.key :as k]
-   [er-cassandra.record :as r]
-   [er-cassandra.record-either :as re]
+   [er-cassandra.record-either :as r]
    [er-cassandra.model.types :as t]
    [er-cassandra.model.util :refer [combine-responses create-lookup-record]])
   (:import [er_cassandra.model.types Model]))
@@ -44,7 +43,7 @@
                   :key key :key-value key-value}]
 
     (m/with-monad dm/either-deferred-monad
-      (m/mlet [insert-response (re/insert session
+      (m/mlet [insert-response (r/insert session
                                           (:name unique-key-table)
                                           unique-key-record
                                           {:if-not-exists true})
@@ -58,7 +57,7 @@
                         insert-response))
 
                live-ref? (if-not owned?
-                           (re/select-one session
+                           (r/select-one session
                                           (get-in model [:primary-table :name])
                                           (get-in model [:primary-table :key])
                                           (t/extract-uber-key-value
@@ -71,7 +70,7 @@
 
                stale-update-response (if (and (not owned?)
                                               (not live-ref?))
-                                       (re/update
+                                       (r/update
                                         session
                                         (:name unique-key-table)
                                         (:key unique-key-table)
@@ -93,13 +92,15 @@
                  :else     [:fail key-desc :notunique] ;; someone else won
                  ))))))
 
-(defn remove-unique-key
+(defn release-unique-key
   "remove a single unique key"
   [session ^Model model unique-key-table uber-key-value key-value]
   (let [uber-key (t/uber-key model)
-        key (:key unique-key-table)]
+        key (:key unique-key-table)
+        key-desc {:uber-key uber-key :uber-key-value uber-key-value
+                  :key key :key-value key-value}]
     (m/with-monad dm/either-deferred-monad
-      (m/mlet [delete-result (re/delete session
+      (m/mlet [delete-result (r/delete session
                                         (:name unique-key-table)
                                         key
                                         key-value
@@ -110,39 +111,128 @@
                deleted? (m/return (applied? delete-result))]
               (m/return
                (cond
-                 deleted? [:ok :deleted]
-                 :else    [:ok :stale]))))))
+                 deleted? [:ok key-desc :deleted]
+                 :else    [:ok key-desc :stale]))))))
 
-(defn acquire-singular-unique-key
-  "attempt to acquire a singular unique key, returning a
-   Deferred[Right[[:ok key info]]] or a Deferred[Right[[:fail key reason]]]"
-  [session ^Model model unique-key-table owner-record]
-  (let [uber-key-value (t/extract-uber-key-value model owner-record)
-        key-value (k/extract-key-value (:key unique-key-table) owner-record)]
-    (acquire-unique-key session model unique-key-table uber-key-value key-value)))
+(defn release-stale-unique-keys
+  [session ^Model model old-record new-record]
+  (combine-responses
+   (mapcat
+    identity
+    (for [t (:unique-key-tables model)]
+      (let [uber-key (t/uber-key model)
+            uber-key-value (t/extract-uber-key-value model new-record)
+            key (:key t)
+            coll (:collection t)]
+        (if coll
+          (let [old-kvs (set (k/extract-key-value-collection key old-record))
+                new-kvs (set (k/extract-key-value-collection key new-record))
+                stale-kvs (filter identity (set/difference new-kvs old-kvs))]
+            (for [kv stale-kvs]
+              (release-unique-key session model t uber-key-value kv)))
 
-(defn acquire-unique-key-collection
-  "attempt to acquire a set of unique keys. returns a
-   Deferred[Right[[ [:ok key info] | [:fail key reason] ]]]"
-  [session ^Model model unique-key-table owner-record]
-  (let [uber-key-value (t/extract-uber-key-value model owner-record)
-        key-value-coll (k/extract-key-value-collection
-                        (:key unique-key-table)
-                        owner-record)
-        responses (mapv (partial acquire-unique-key
-                                 session
+          (let [old-kv (k/extract-key-value key old-record)
+                new-kv (k/extract-key-value key new-record)]
+            (when (and old-kv
+                       (not= old-kv new-kv))
+              [(release-unique-key session model t uber-key-value old-kv)]))))))))
+
+(defn acquire-unique-keys
+  [session ^Model model record]
+  (combine-responses
+   (mapcat
+    identity
+    (for [t (:lookup-tables model)]
+      (let [uber-key (t/uber-key model)
+            uber-key-value (t/extract-uber-key-value model record)
+            key (:key t)
+            coll (:collection t)]
+        (if coll
+          (let [kvs (filter identity
+                            (set (k/extract-key-value-collection key record)))]
+            (for [kv kvs]
+              (let [lookup-record (create-lookup-record
+                                   uber-key uber-key-value
+                                   key kv)]
+                (acquire-unique-key session
+                                    model
+                                    t
+                                    uber-key-value
+                                    kv))))
+
+          (when-let [key-value (k/extract-key-value key record)]
+            [(acquire-unique-key session
                                  model
-                                 unique-key-table
-                                 uber-key-value)
-                        key-value-coll)]
-    (combine-responses responses)))
+                                 t
+                                 uber-key-value
+                                 key-value)])))))))
 
+(defn update-with-acquire-responses
+  [table acquire-key-responses record]
+  (reduce (fn [[status key-desc _]]
+            (let [coll (:collection table)
+                  key-col (last (:key key-desc))
+                  key-val (last (:key-value key-desc))]
+              (condp = coll
+                :set (when-not (= :ok status)
+                       (assoc record
+                              key-col
+                              (disj (get record key-col)
+                                    key-val)))
+                :list (when-not (= :ok status)
+                        (assoc record
+                               key-col
+                               (filterv #(not= key-val %)
+                                        (get record key-col))))
+
+                :map (when-not (= :ok status)
+                       (assoc record
+                              key-col
+                              (dissoc (get record key-col)
+                                      key-val)))
+
+                :else (when-not (= :ok status)
+                        (assoc record key-col nil)))))
+          record
+          acquire-key-responses))
+
+(defn responses-for-key
+  [match-key responses]
+  (filter (fn [[_ {:keys [key]} _]]
+            (= key match-key))
+          responses))
+
+(defn update-record-by-key-responses
+  [model old-record new-record acquire-key-responses]
+
+  (reduce (fn [nr t]
+            (let [ars (responses-for-key (:key t) acquire-key-responses)]
+              (update-with-acquire-responses t ars nr)))
+          new-record
+          (:unique-key-tables model)))
 
 (defn acquire-unique-keys
   "attempts to acquire unique keys for an owner... returns
    a Deferred[Right[[:ok updated-owner-record :failed-keys]]] with an updated
    owner record containing only the keys that could be acquired"
-  [session ^Model model owner-record]
+  [session ^Model model new-record]
   (m/with-monad dm/either-deferred-monad
-    (m/mlet [r owner-record]
-            (m/return r))))
+    (m/mlet [old-record (r/select-one session
+                                      (get-in model [:primary-table :name])
+                                      (get-in model [:primary-table :key])
+                                      (t/extract-uber-key-value model new-record))
+
+             release-key-responses (release-stale-unique-keys
+                                    session
+                                    model
+                                    old-record
+                                    new-record)
+             acquire-key-responses (acquire-unique-keys
+                                    session
+                                    model
+                                    new-record)]
+            (m/return
+             (update-record-by-key-responses
+              old-record
+              new-record
+              acquire-key-responses)))))
