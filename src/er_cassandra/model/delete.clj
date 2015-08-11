f66d812b8d88d568ae77b773877f27cac4a9be22
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-11 16:42:18 +0100

    delete model instance

--- src/er_cassandra/model/delete.clj
+++ src/er_cassandra/model/delete.clj
@@ -1,22 +1,57 @@
 (ns er-cassandra.model.delete
-  (:require [manifold.deferred :as d]
-            [qbits.alia :as alia]
-            [qbits.alia.manifold :as aliam]
-            [qbits.hayt :as h]
-            [er-cassandra.key :as k]
-            [er-cassandra.record :as r]
-            [er-cassandra.model.types]
-            [er-cassandra.model.select :refer [select-one]])
+  (:require
+   [clojure.set :as set]
+   [clojure.core.match :refer [match]]
+   [manifold.deferred :as d]
+   [cats.core :as m]
+   [cats.monad.either :as either]
+   [cats.monad.deferred :as dm]
+   [qbits.alia :as alia]
+   [qbits.alia.manifold :as aliam]
+   [qbits.hayt :as h]
+   [er-cassandra.key :as k]
+   [er-cassandra.record-either :as r]
+   [er-cassandra.model.types :as t]
+   [er-cassandra.model.util :refer [combine-responses create-lookup-record]]
+   [er-cassandra.model.unique-key :as unique-key]
+   [er-cassandra.model.upsert :as upsert]
+   [er-cassandra.model.select :as select])
   (:import [er_cassandra.model.types Model]))
 
 (defn delete
-  "delete a single instance, removing primary, secondary and lookup records "
+  "delete a single instance, removing primary, secondary unique-key and
+   lookup records "
 
   ([session ^Model model key record-or-key-value]
-   (delete session model key record-or-key-value {}))
 
-  ([session ^Model model key record-or-key-value opts]
-   ;; delete from primary-table where uber-key=X
-   ;; delete from each secondary table where uber-key=X
-   ;; delete from each lookup table where uber-key=X
-   ))
+   (m/with-monad dm/either-deferred-monad
+     (m/mlet [record (select/select-one session
+                                        model
+                                        key
+                                        record-or-key-value)
+
+              primary-response (upsert/delete-record
+                                session
+                                model
+                                (:primary-table model)
+                                (t/extract-uber-key-value model record))
+
+              unique-responses (unique-key/release-stale-unique-keys
+                                session
+                                model
+                                record
+                                nil)
+
+              secondary-responses (upsert/delete-stale-secondaries
+                                   session
+                                   model
+                                   record
+                                   nil)
+
+              lookup-responses (upsert/delete-stale-lookups
+                                session
+                                model
+                                record
+                                nil)]
+             (m/return
+              [:ok record :deleted])))))
