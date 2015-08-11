2d3b1e7d3ebb5287563d44f580e723bffadc3aed
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-11 14:42:50 +0100

    high-level upsert

--- src/er_cassandra/model/upsert.clj
+++ src/er_cassandra/model/upsert.clj
@@ -1,5 +1,6 @@
 (ns er-cassandra.model.upsert
   (:require
+   [clojure.set :as set]
    [clojure.core.match :refer [match]]
    [manifold.deferred :as d]
    [cats.core :as m]
@@ -11,179 +12,142 @@
    [er-cassandra.key :as k]
    [er-cassandra.record :as r]
    [er-cassandra.record-either :as re]
-   [er-cassandra.model.types :as t])
+   [er-cassandra.model.types :as t]
+   [er-cassandra.model.util :refer [combine-responses create-lookup-record]]
+   [er-cassandra.model.unique-key :as uk])
   (:import [er_cassandra.model.types Model]))
 
-(defn applied?
-  [lwt-response]
-  (get lwt-response (keyword "[applied]")))
-
-(defn applied-or-owned?
-  [^Model model insert-uber-key lwt-insert-response ]
-  (if (applied? lwt-insert-response)
-    true
-    (let [owner-uber-key (k/extract-key-value
-                          (get-in model [:primary-table :key])
-                          lwt-insert-response)]
-      (= insert-uber-key owner-uber-key))))
-
-(defn unique-key-table-upsert
-  "insert into a unique-key table with a lightweight transaction.
-   returns a Deferred[Either], with Right values describing how
-   they were successful, and Left values describing errors"
-
-  [session ^Model model unique-key-table unique-key-record]
-
+(defn delete-record
+  [session model table key-value]
   (m/with-monad dm/either-deferred-monad
-    (m/mlet [insert-response (re/insert session
-                                        (:name unique-key-table)
-                                        unique-key-record
-                                        {:if-not-exists true})
-
-             inserted? (m/return (applied? insert-response))
-
-             owned? (m/return
-                      (applied-or-owned?
-                       model
-                       (t/extract-uber-key-value model unique-key-record)
-                       insert-response))
-
-             live-ref? (if-not owned?
-                         (re/select-one session
-                                        (get-in model [:primary-table :name])
-                                        (get-in model [:primary-table :key])
-                                        (t/extract-uber-key-value
-                                         model
-                                         insert-response))
-                         (m/return nil))
-
-             stale-update-response (if (and (not owned?)
-                                            (not live-ref?))
-                                     (re/update
-                                      session
-                                      (:name unique-key-table)
-                                      (:key unique-key-table)
-                                      unique-key-record
-                                      {:only-if
-                                       (t/extract-uber-key-equality-clause
-                                        model
-                                        insert-response)})
-                                     (m/return nil))
-
-             updated? (m/return (applied? stale-update-response))]
-
+    (m/mlet [delete-result (re/delete session
+                                      (:name table)
+                                      (:key table)
+                                      key-value)]
             (m/return
-             (cond
-               inserted? [:ok :inserted]    ;; new key
-               owned?    [:ok :owned]       ;; ours already
-               updated?  [:ok :updated]     ;; ours now
-               live-ref? [:fail :notunique] ;; not ours
-               :else     [:fail :notunique] ;; someone else won
-               )))))
+             [:ok {:table (:name table)
+                   :key (:key table)
+                   :key-value key-value} :deleted]))))
 
-(defn acquire-unique-key
-  "acquire a single unique key.
-   returns a Deferred[Right[:ok <keydesc> info]] if the key was acquired
-   successfully, a Deferred[Right[:fail <keydesc> reason]]"
-  [session ^Model model unique-key-table uber-key-value key-value]
-  (let [uber-key (t/uber-key model)
-        key (:key unique-key-table)
-        unique-key-record (into {}
-                                (concat (map vector uber-key uber-key-value)
-                                        (map vector key key-value)))]
-    (m/with-monad dm/either-deferred-monad
-      (m/mlet [acquire-result (unique-key-table-upsert session
-                                                       model
-                                                       unique-key-table
-                                                       unique-key-record)]
-              (m/return
-               (match acquire-result
-                      [:ok info] [:ok
-                                  {:uber-key uber-key
-                                   :uber-key-value uber-key-value
-                                   :key key
-                                   :key-value key-value}
-                                  info]
-                      [_ reason] [:fail
-                                  {:uber-key uber-key
-                                   :uber-key-value uber-key-value
-                                   :key key
-                                   :key-value key-value}
-                                  reason]))))))
-
-(defn remove-unique-key
-  "remove a single unique key"
-  [session ^Model model unique-key-table uber-key-value key-value]
-  (let [uber-key (t/uber-key model)
-        key (:key unique-key-table)]
-    (m/with-monad dm/either-deferred-monad
-      (m/mlet [delete-result (re/delete session
-                                        (:name unique-key-table)
-                                        key
-                                        key-value
-                                        {:only-if
-                                         (k/key-equality-clause
-                                          uber-key
-                                          uber-key-value)})
-               deleted? (m/return (applied? delete-result))]
-              (m/return
-               (cond
-                 deleted? [:ok :deleted]
-                 :else    [:ok :stale]))))))
-
-(defn acquire-singular-unique-key
-  "attempt to acquire a singular unique key, returning a
-   Deferred[Right[[:ok key info]]] or a Deferred[Right[[:fail key reason]]]"
-  [session ^Model model unique-key-table owner-record]
-  (let [uber-key-value (t/extract-uber-key-value model owner-record)
-        key-value (k/extract-key-value (:key unique-key-table) owner-record)]
-    (acquire-unique-key session model unique-key-table uber-key-value key-value)))
-
-(defn acquire-unique-key-collection
-  "attempt to acquire a set of unique keys. returns a
-   Deferred[Right[[ [:ok key info] | [:fail key reason] ]]]"
-  [session ^Model model unique-key-table owner-record]
-  (let [uber-key-value (t/extract-uber-key-value model owner-record)
-        key-value-coll (k/extract-key-value-collection
-                        (:key unique-key-table)
-                        owner-record)
-        responses (mapv (partial acquire-unique-key
-                                 session
-                                 model
-                                 unique-key-table
-                                 uber-key-value)
-                        key-value-coll)]
-    (d/chain
-     (apply d/zip responses)
-     (fn [responses]
-       (clojure.pprint/pprint responses)
-       (if (some either/left? responses)
-         (either/left responses)
-
-         (either/right (mapv deref responses)))))))
-
-(defn lookup-key-table-upsert
-  [session ^Model model lookup-key-table record]
+(defn upsert-record
+  [session ^Model model table record]
   (m/with-monad dm/either-deferred-monad
-    (m/mlet [upsert-response (re/insert session
-                                        (:name lookup-key-table)
-                                        record)]
-            (m/return :upserted))))
+    (m/mlet [insert-result (re/insert session
+                                      (:name table)
+                                      record)]
+            (m/return
+             [:ok record :upserted]))))
 
+(defn delete-stale-secondaries
+  [session ^Model model old-record new-record]
+  (combine-responses
+   (filter
+    identity
+    (for [t (:secondary-tables model)]
+      (let [key (:key t)
+            old-key-value (k/extract-key-value key old-record)
+            new-key-value (k/extract-key-value key new-record)]
+        (when (and old-key-value
+                   (not= old-key-value new-key-value))
+          (delete-record session
+                         model
+                         t
+                         old-key-value)))))))
 
+(defn upsert-secondaries
+  "returns Deferred[Right[]]"
+  [session ^Model model record]
+  (combine-responses
+   (for [t (:secondary-tables model)]
+     (upsert-record session model t record))))
 
+(defn delete-stale-lookups
+  [session ^Model model old-record new-record]
+  (combine-responses
+   (mapcat
+    identity
+    (for [t (:lookup-tables model)]
+      (let [uber-key (t/uber-key model)
+            uber-key-value (t/extract-uber-key-value model new-record)
+            key (:key t)
+            coll (:collection t)]
 
+        (if coll
+          (let [old-kvs (set (k/extract-key-value-collection key old-record))
+                new-kvs (set (k/extract-key-value-collection key new-record))
+                stale-kvs (filter identity (set/difference new-kvs old-kvs))]
+            (for [kv stale-kvs]
+              (delete-record session model t kv)))
 
+          (let [old-kv (k/extract-key-value key old-record)
+                new-kv (k/extract-key-value key new-record)]
+            (when (and old-kv
+                       (not= old-kv new-kv))
+              [(delete-record session model t old-kv)]))))))))
+
+(defn upsert-lookups
+  [session ^Model model record]
+  (combine-responses
+   (mapcat
+    identity
+    (for [t (:lookup-tables model)]
+      (let [uber-key (t/uber-key model)
+            uber-key-value (t/extract-uber-key-value model record)
+            key (:key t)
+            coll (:collection t)]
+        (if coll
+          (let [kvs (filter identity
+                            (set (k/extract-key-value-collection key record)))]
+            (for [kv kvs]
+              (let [lookup-record (create-lookup-record
+                                   uber-key uber-key-value
+                                   key kv)]
+                (upsert-record session
+                               model
+                               t
+                               lookup-record))))
+
+          (let [key-value (k/extract-key-value key record)]
+            (when-let [lookup-record (create-lookup-record
+                                      uber-key uber-key-value
+                                      key key-value)]
+              [(upsert-record session model t lookup-record)]))))))))
 
 (defn upsert
-  "upsert a single instance, creating primary, secondary, unique-key and
-   lookup records as required"
+  "upsert a single instance, upserting primary, secondary, unique-key and
+   lookup records as required and deleting stale secondary, unique-key and
+   lookup records"
 
   ([session ^Model model record]
-   (upsert session model record {}))
 
-  ([session ^Model model record opts]
-   ;; insert to primary table if not exists
-   ;; insert to secondary tables if not exists
-   ;; insert to each lookup-table if not exists
-   ))
+   (m/with-monad dm/either-deferred-monad
+     (m/mlet [with-unique-keys (uk/acquire-unique-keys session model record)
+
+              stale-secondary-responses (delete-stale-secondaries
+                                         session
+                                         model
+                                         with-unique-keys
+                                         record)
+
+              stale-lookup-responses (delete-stale-lookups
+                                      session
+                                      model
+                                      with-unique-keys
+                                      record)
+
+              secondary-reponses (combine-responses
+                                  (upsert-secondaries
+                                   session
+                                   model
+                                   record))
+
+              lookup-responses (combine-responses
+                                (upsert-lookups
+                                 session
+                                 model
+                                 record))]
+             (m/return
+              (re/select-one session
+                             (get-in model [:primary-table :name])
+                             (get-in model [:primary-table :key])
+                             (t/extract-uber-key-value model record)))))))
