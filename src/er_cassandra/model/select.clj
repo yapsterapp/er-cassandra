7d7bd7da6717dae3852438942fcc016b0afe3e5a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-28 12:30:05 +0100

    support where clauses with partition keys
    it's now possible to supply a partition key and additional where
    clauses on clustering columns

--- src/er_cassandra/model/select.clj
+++ src/er_cassandra/model/select.clj
@@ -7,7 +7,8 @@
             [qbits.hayt :as h]
             [er-cassandra.key :as k]
             [er-cassandra.record :as r]
-            [er-cassandra.model.types :as t])
+            [er-cassandra.model.types :as t]
+            [er-cassandra.model.util :as util])
   (:import [er_cassandra.model.types Model]))
 
 (defn if-primary-key-table
@@ -27,42 +28,68 @@
 (defn if-unique-key-table
   [^Model model key]
   (some (fn [t]
-          (when (= (:key t) key)
+          (when (or (t/satisfies-primary-key? (:key t) key)
+                    (t/satisfies-partition-key? (:key t) key))
             t))
         (:unique-key-tables model)))
 
 (defn if-lookup-key-table
   [^Model model key]
   (some (fn [t]
-          (when (= (:key t) key)
+          (when (or (t/satisfies-primary-key? (:key t) key)
+                    (t/satisfies-partition-key? (:key t) key))
             t))
         (:lookup-key-tables model)))
 
 (defn select-from-full-table
-  "one fetch - straight from a table"
+  "one fetch - straight from a table. they key must be either
+   a full primary key, or a partition key combined with some
+   clustering key conditions (given as :where options)"
 
   [session ^Model model table key record-or-key-value opts]
-  (let [kv (k/extract-key-value key record-or-key-value opts)]
-    (r/select session (:name table) key kv (dissoc opts :key-value))))
+  (let [kv (k/extract-key-value key record-or-key-value opts)
+        opts (dissoc opts :key-value)]
+    (r/select session (:name table) key kv opts)))
 
 (defn select-from-lookup-table
   "two fetches - use the lookup-key to get the uber-key, then
-   get the record from the primary table"
+   get the record from the primary table.
+
+   the lookup query may return multiple records, all of which will be
+   dereferenced against the primary table, with nil results from
+   dangling lookups being filtered out.
+
+   this means that the lookup query may specify a partition-key and
+   some clustering column condition (given as a :where option)"
 
   [session ^Model model table record-or-key-value opts]
-  (let [lkv (k/extract-key-value (:key table) record-or-key-value opts)]
+  (let [lkv (k/extract-key-value (:key table) record-or-key-value opts)
+        opts (dissoc opts :key-value)
+        lookup-opts (dissoc opts :columns)
+        primary-opts (dissoc opts :where :only-if :order-by :limit)]
     (m/with-monad dm/deferred-monad
-      (m/mlet [lr (r/select-one session (:name table) (:key table) lkv)
-               pkv (m/return
-                    (when lr
-                      (t/extract-uber-key-value model lr)))]
-              (if pkv
-                (r/select session
-                          (get-in model [:primary-table :name])
-                          (get-in model [:primary-table :key])
-                          pkv
-                          (dissoc opts :key-value))
-                (m/return nil))))))
+      (m/mlet [lrs (r/select session
+                             (:name table)
+                             (:key table)
+                             lkv
+                             lookup-opts)
+               pkvs (m/return
+                     (map (fn [lr]
+                            (t/extract-uber-key-value model lr))
+                          lrs))
+               prs (m/return
+                    (map (fn [pkv]
+                           (r/select-one
+                            session
+                            (get-in model [:primary-table :name])
+                            (get-in model [:primary-table :key])
+                            pkv
+                            primary-opts))
+                         pkvs))]
+        (m/return
+         (d/chain
+          (util/combine-responses prs)
+          (fn [rs] (filter identity rs))))))))
 
 (defn select*
   "select records from primary or lookup tables as required"
