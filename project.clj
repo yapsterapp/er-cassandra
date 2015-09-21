90047e77f25db1780fcff1f27ec4d8186b482cd8
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-09-21 01:22:26 +0100

    v0.5.0

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
-(defproject employeerepublic/er-cassandra "0.4.9"
+(defproject employeerepublic/er-cassandra "0.5.0"
   :description "a simple cassandra conector"
   :url "https://github.com/employeerepublic/er-cassandra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.cli "0.3.2"]
                  [org.clojure/core.match "0.2.2"]
+                 [org.clojure/math.combinatorics "0.1.1"]
                  [potemkin "0.4.1"]
                  [prismatic/plumbing "0.4.4"]
                  [clj-time "0.10.0"]
                  [cc.qbits/alia "2.7.3" ] ;; :exclusions [com.google.guava/guava]
                  [drift "1.5.3" :exclusions [org.clojure/clojure]]
+                 [manifold "0.1.1-alpha4"]
                  [mccraigmccraig/cats "1.1.0-20150920.154602-1"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
