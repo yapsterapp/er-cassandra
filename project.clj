bd9819e4274b1a13a154cdc60c90d45a648a1ebc
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-01-11 14:37:12 +0000

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,21 +1,29 @@
-(defproject employeerepublic/er-cassandra "0.7.5"
+(defproject employeerepublic/er-cassandra "0.8.0"
   :description "a simple cassandra conector"
   :url "https://github.com/employeerepublic/er-cassandra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.cli "0.3.2"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+
+  :pedantic? :abort
+
+  :dependencies [[org.clojure/tools.cli "0.3.3"
+                  :exclusions [org.clojure/clojure]]
+                 [org.clojure/core.match "0.3.0-alpha4"
+                  :exclusions [org.clojure/tools.analyzer.jvm]]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [potemkin "0.4.1"]
-                 [prismatic/plumbing "0.4.4"]
-                 [clj-time "0.10.0"]
-                 [cc.qbits/alia "2.12.0"] ;; :exclusions [com.google.guava/guava]
-                 [environ "1.0.0"]
-                 [drift "1.5.3" :exclusions [org.clojure/clojure]]
-                 [manifold "0.1.1"]
-                 [mccraigmccraig/cats "1.1.0-20150920.154602-1"]]
+                 [potemkin "0.4.3"]
+                 [prismatic/plumbing "0.5.2"]
+                 [clj-time "0.11.0"
+                  :exclusions [org.clojure/clojure]]
+                 [cc.qbits/alia "3.0.0-beta1"
+                  :exclusions [org.clojure/clojure]] ;; :exclusions [com.google.guava/guava]
+                 [environ "1.0.1"
+                  :exclusions [org.clojure/clojure]]
+                 [drift "1.5.3"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/tools.logging]]
+                 [manifold "0.1.2-alpha2"]
+                 [funcool/cats "1.2.1"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [expectations "2.1.2" :exclusions [com.google.guava/guava]]]
-
-                   :plugins [[lein-expectations "0.0.8"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]
+                   :plugins []}})
