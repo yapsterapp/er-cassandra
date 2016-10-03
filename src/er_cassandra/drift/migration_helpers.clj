e5bb30f2827cc038fdd375e59cd157b19ba8955e
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-10-03 14:50:13 +0100

    cqlsh migration helper

--- src/er_cassandra/drift/migration_helpers.clj
+++ src/er_cassandra/drift/migration_helpers.clj
@@ -1,6 +1,8 @@
 (ns er-cassandra.drift.migration-helpers
   (:require [drift.config :as config]
-            [er-cassandra.session]))
+            [er-cassandra.session]
+            [clojure.java.shell :refer [sh]]
+            [environ.core :refer [env]]))
 
 (defn session
   "get the alia session"
@@ -16,3 +18,8 @@
   []
   (println "pausing to let cassandra catch up")
   (Thread/sleep 5000))
+
+(defn cqlsh
+  [cqlsh-cmd]
+  (let [cqlsh (env :cqlsh "cqlsh")]
+    (sh cqlsh :in cqlsh-cmd)))
