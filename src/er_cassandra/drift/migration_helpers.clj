2110d1b06acad1b1d06fb0cd85ab652d4db8218a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-10-04 16:39:05 +0100

    cqlsh is now deferred

--- src/er_cassandra/drift/migration_helpers.clj
+++ src/er_cassandra/drift/migration_helpers.clj
@@ -2,7 +2,11 @@
   (:require [drift.config :as config]
             [er-cassandra.session]
             [clojure.java.shell :refer [sh]]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [cats.core :refer [mlet return]]
+            [cats.context :refer [with-context]]
+            [manifold.deferred :as d]
+            [cats.labs.manifold :refer [deferred-context]]))
 
 (defn session
   "get the alia session"
@@ -21,5 +25,19 @@
 
 (defn cqlsh
   [cqlsh-cmd]
-  (let [cqlsh (env :cqlsh "cqlsh")]
-    (sh cqlsh :in cqlsh-cmd)))
+  (with-context deferred-context
+    (mlet [{sh-exit :exit
+            sh-out :out
+            sh-err :err
+            :as sh-r} (d/future
+                           (sh (env :cqlsh "cqlsh")
+                               :in cqlsh-cmd))]
+      ;; cqlsh seems to return code 0 with a non-empty error
+      ;; instead of non-zero error-codes
+      (if (and (= 0 sh-exit)
+               (empty? sh-err))
+        (return sh-out)
+        (throw (ex-info "sh error" {:cqlsh-cmd cqlsh-cmd
+                                    :exit sh-exit
+                                    :out sh-out
+                                    :err sh-err}))))))
