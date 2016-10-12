88594f56623811dcde6b92c06af408249e372a65
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-10-12 16:19:59 +0100

    document the model-session

--- src/er_cassandra/model/model_session.clj
+++ src/er_cassandra/model/model_session.clj
@@ -5,11 +5,20 @@
    [er_cassandra.model.types Model]))
 
 (defprotocol ModelSession
-  (-record-session [this])
-  (-select [this ^Model model key record-or-key-value opts])
-  (-upsert [this ^Model model record opts])
-  (-delete [this ^Model model key record-or-key-value opts])
-  (-close [this]))
+  (-record-session [this]
+    "return the low-level record session")
+
+  (-select [this ^Model model key record-or-key-value opts]
+    "select model instances")
+
+  (-upsert [this ^Model model record opts]
+    "upsert a single model instance")
+
+  (-delete [this ^Model model key record-or-key-value opts]
+    "delete a single model instance")
+
+  (-close [this]
+    "close the session, releasing any resources"))
 
 (defprotocol ModelSpySession
   (-model-spy-log [this])
