c02e2cc244eebb6f022e13d4744e7c4536b16512
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-11-23 13:04:53 +0000

    get missing with-cols from old-record for lookups

--- test/er_cassandra/model/alia/upsert_test.clj
+++ test/er_cassandra/model/alia/upsert_test.clj
@@ -63,15 +63,28 @@
   (let [m (t/create-model
            {:primary-table {:name :foos :key [:id]}
             :secondary-tables [{:name :foos_by_bar :key [:bar]}
-                               {:name :foos_by_baz
-                                :key [:baz]}]
+                               {:name :foos_by_baz :key [:baz]}]
             :lookup-key-tables [{:name :foos_by_x
                                  :key [:x]
-                                 :with-columns [:c1 :c2]}]})
-        record {:id 1 :bar "bar1" :baz "baz1" :c1 :C1 :c2 :C2 :c3 :C3 :x "x-key"}
-        [t lrecord] (first (u/lookup-record-seq m record))]
-    (are [x y] (= x y)
-      1      (count (u/lookup-record-seq m record))
-      :C1    (:c1 lrecord)
-      :C2    (:c2 lrecord)
-      false  (contains? lrecord :c3))))
+                                 :with-columns [:c1 :c2]}]})]
+
+    (testing "lookup-keys with a full record supplied"
+      (let [record {:id 1 :bar "bar1" :baz "baz1" :c1 :C1 :c2 :C2 :c3 :C3 :x "x-key"}
+            [[t lrecord] & _ :as lookups] (u/lookup-record-seq m nil record)]
+
+        (are [x y] (= x y)
+          1      (count lookups)
+          :C1    (:c1 lrecord)
+          :C2    (:c2 lrecord)
+          false  (contains? lrecord :c3))))
+
+    (testing "lookup-keys with a partial record supplied"
+      (let [old-record {:id 1 :bar "bar1" :baz "baz1" :x "x-key" :c1 :C1 :c2 :C2 :c3 :C3 }
+            record {:id 1 :bar "bar1" :baz "baz1" :x "x-key" :c1 :C1 }
+            [[t lrecord] & _ :as lookups] (u/lookup-record-seq m old-record record)]
+
+        (are [x y] (= x y)
+          1      (count lookups)
+          :C1    (:c1 lrecord)
+          :C2    (:c2 lrecord)
+          false  (contains? lrecord :c3))))))
