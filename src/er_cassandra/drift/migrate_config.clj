5dae3a556cd22fd8a29d6ff4213f9d5fd78b76d6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-11-25 15:38:38 +0000

    cassandra changes to support embedded migrations

--- src/er_cassandra/drift/migrate_config.clj
+++ src/er_cassandra/drift/migrate_config.clj
@@ -4,10 +4,10 @@
    [er-cassandra.drift.migrations :as m]))
 
 (defnk cassandra
-  [session keyspace namespace directory]
+  [session keyspace namespace directory {other-config nil}]
 
   {:directory directory
-   :init (m/create-init-fn session keyspace namespace)
+   :init (m/create-init-fn session keyspace namespace other-config)
    :current-version m/current-version
    :update-version m/update-version
    :ns-content m/ns-content
