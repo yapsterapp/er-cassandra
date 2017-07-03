a9ab7d0f380b410100d6a63ff1c635603af790c1
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-07-03 14:30:11 +0100

    adam harper interview test-code - seems legit

--- test/er_cassandra/model/types_test.clj
+++ test/er_cassandra/model/types_test.clj
@@ -1,6 +1,8 @@
 (ns er-cassandra.model.types-test
   (:require
-   [clojure.test :as test :refer [deftest is are use-fixtures]]
+   [clojure.test :as test :refer [deftest is are use-fixtures testing]]
+   [clojure.string :as string]
+   [manifold.deferred :as deferred]
    [schema.test :as st]
    [er-cassandra.model.types :as t]
    [er-cassandra.model.types :refer :all]))
@@ -62,3 +64,28 @@
                                  :view? true}]})]
     (is (= (->> m :lookup-tables (take 1))
            (t/mutable-lookup-tables m)))))
+
+
+(deftest test-run-callbacks
+  (let [s nil
+        m (t/create-entity
+           {:primary-table {:name :foos :key [:id]}})
+        update-callbacks (fn [m cbs]
+                           (assoc-in m [:callbacks :before-save] cbs))
+        r {:id 1 :name "bar"}]
+      (let [m (update-callbacks m [identity])]
+    (testing "identity"
+        (is (= r @(run-callbacks s m :before-save r)))))
+    (testing "callback sequence"
+      (let [m (update-callbacks m [#(assoc % :name "baz")
+                                   #(update % :name string/upper-case)])
+            exp {:id 1 :name "BAZ"}]
+        (is (= exp @(run-callbacks s m :before-save r)))))
+    (testing "callback deferred"
+      (let [m (update-callbacks m [(reify ICallback
+                                     (run-callback [_ session entity record]
+                                       (deferred/success-deferred
+                                         (update record :name string/upper-case))))])
+            exp {:id 1 :name "BAR"}]
+        (is (= exp @(run-callbacks s m :before-save r)))))
+    (testing "callback error")))
