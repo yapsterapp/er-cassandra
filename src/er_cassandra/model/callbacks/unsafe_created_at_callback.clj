c844368f3f3cd03cd6492bbde1321507cda85d15
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-09-06 19:57:05 +0100

    callbacks for created_at_timeuuid

--- src/er_cassandra/model/callbacks/unsafe_created_at_callback.clj
+++ src/er_cassandra/model/callbacks/unsafe_created_at_callback.clj
@@ -1,6 +1,7 @@
 (ns er-cassandra.model.callbacks.unsafe-created-at-callback
   (:require
-   [clj-time.core :as t]))
+   [clj-time.core :as t]
+   [clj-uuid :as uuid]))
 
 (defn unsafe-created-at-callback
   "it's sort of kinda like a created-at, but it's not very safe,
@@ -12,3 +13,20 @@
        (if v
          r
          (assoc r col (.toDate (t/now))))))))
+
+(defn unsafe-created-at-timeuuid-callback
+  ([] (unsafe-created-at-timeuuid-callback :created_at_timeuuid))
+  ([col]
+   (fn [r]
+     (let [v (get r col)]
+       (if v
+         r
+         (assoc r col (uuid/v1)))))))
+
+(defn timestamp-from-timeuuid-callback
+  [timeuuid-col timestamp-col]
+  (fn [r]
+    (let [t-uuid (get r timeuuid-col)]
+      (if (uuid/uuid? t-uuid)
+        (assoc r timestamp-col (uuid/get-instant t-uuid))
+        r))))
