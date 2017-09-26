ecb83acb283c2121b351187ab8e276ccc7f29aec
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-09-26 12:23:47 +0200

    extend concatenate-keys to encode Insts sortably

--- src/er_cassandra/model/callbacks/concatenate_keys_callback.clj
+++ src/er_cassandra/model/callbacks/concatenate_keys_callback.clj
@@ -1,31 +1,18 @@
 (ns er-cassandra.model.callbacks.concatenate-keys-callback
   (:require
+   [er-cassandra.concatenated-key :as ck]
    [clojure.string :as str]))
-
-(def default-separator "/")
-
-(defn concatenate-keys
-  "concatenate some key values with SQL NULL semantics - any nil
-   key-col-val results in a nil result"
-  ([key-col-vals]
-   (concatenate-keys default-separator key-col-vals))
-
-  ([separator key-col-vals]
-   (when (every? some? key-col-vals)
-     (->> key-col-vals
-          (map str)
-          (str/join separator)))))
 
 (defn concatenate-keys-callback
   "callback to synthesize a new key by concatenating other keys,
    with SQL NULL semantics"
   ([to-col key-cols]
-   (concatenate-keys-callback to-col default-separator key-cols))
+   (concatenate-keys-callback to-col ck/default-separator key-cols))
 
   ([to-col separator key-cols]
    (fn [r]
      (let [key-col-vals (->> key-cols
                              (map #(get r %)))
-           to-col-val (concatenate-keys separator key-col-vals)]
+           to-col-val (ck/concatenate-keys separator key-col-vals)]
 
        (assoc r to-col to-col-val)))))
