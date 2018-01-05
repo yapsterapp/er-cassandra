3a6319b3c835dedb9bf9992b00badeb9b13ccc3a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2018-01-05 12:12:30 +0000

    record-layer :prepare? test

--- test/er_cassandra/record_test.clj
+++ test/er_cassandra/record_test.clj
@@ -31,3 +31,27 @@
                 t)]
         (is (= nil
                @(r/select-one tu/*session* :stmt_test :id t)))))))
+
+
+(deftest prepared-insert-select-delete-test
+  (let [_ (tu/create-table :stmt_test "(id timeuuid primary key)")
+
+        t (uuid/v1)
+        ;; insert with record layer to trigger truncates on session close
+        _ @(r/insert
+            tu/*session*
+            :stmt_test
+            {:id t}
+            {:prepare? true})]
+    (testing "simple select-one"
+      (is (= {:id t}
+             @(r/select-one tu/*session* :stmt_test :id t {:prepare? true}))))
+    (testing "delete"
+      (let [_ @(r/delete
+                tu/*session*
+                :stmt_test
+                :id
+                t
+                {:prepare? true})]
+        (is (= nil
+               @(r/select-one tu/*session* :stmt_test :id t {:prepare? true})))))))
