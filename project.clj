3fee956ded559ce16cdb0aa80037bf1c391af0c0
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2018-02-13 15:31:49 +0000

    0.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject employeerepublic/er-cassandra "0.11"
+(defproject employeerepublic/er-cassandra "0.12"
   :description "a simple cassandra conector"
   :url "https://github.com/employeerepublic/er-cassandra"
   :license {:name "Eclipse Public License"
