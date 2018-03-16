73676c8253f0010c0550fc257f0b3f7cbc183875
Adam Harper <adam@yapster.info>
2018-03-16 11:02:34 +0000

    cassandra: add schema-helper fns create-table and drop-table

--- src/er_cassandra/migrations/schema_helpers.clj
+++ src/er_cassandra/migrations/schema_helpers.clj
@@ -18,11 +18,47 @@
   [{v-name :name}]
   (string/join " " ["drop materialized view if exists" (name v-name)]))
 
+(defn drop-table
+  [{v-name :name}]
+  (string/join " " ["drop table if exists" (name v-name)]))
+
 (def leveled-compaction-clause
   "compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'tombstone_compaction_interval': '86400'}")
 
 (def size-tiered-compaction-clause
   "compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}")
+
+(defn create-table
+  [{v-name :name
+    v-primary-key :primary-key
+    v-columns :columns
+    v-compaction :compaction
+    :as table-definition}]
+  (assert (every? some? [v-name v-primary-key v-columns])
+          (str "invalid table definition"
+               "\n\n"
+               (pr-str table-definition)))
+  (assert (every? #(contains? (set (map first v-columns)) %) (flatten v-primary-key))
+          (str "not all primary key columns are defined!"
+               "\n\n"
+               (pr-str table-definition)))
+  (assert (or (nil? v-compaction) (#{:leveled :size-tiered} v-compaction)))
+  (let [v-name (name v-name)
+        v-columns (map (fn [[n t]] (str n " " t)) v-columns)
+        primary-key-str (str "primary key " (index-str v-primary-key))]
+    (string/join
+     " "
+     ["create table if not exists" v-name "("
+      (string/join
+       ", "
+       (conj
+        v-columns
+        primary-key-str))
+      ")"
+      (case v-compaction
+        nil nil
+        :leveled (str "WITH " leveled-compaction-clause)
+        :size-tiered (str "WITH " size-tiered-compaction-clause))])))
 
 (defn create-view
   [{v-name :name
