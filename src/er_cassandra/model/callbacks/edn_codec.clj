82d4192fcc78cddf365ba93a4f9d0a597bfc0be1
Roland Sadowski <szabla@gmail.com>
2018-06-14 14:57:49 +0100

    cassandra, model: edn-codec accepts optional readers map
    - needed to be able to store the mock RMS in the db

--- src/er_cassandra/model/callbacks/edn_codec.clj
+++ src/er_cassandra/model/callbacks/edn_codec.clj
@@ -18,16 +18,18 @@
         :else (update r col pr-str)))))
 
 (defn edn-deserialize-callback
-  ([col] (edn-deserialize-callback col nil))
-  ([col default-value]
+  ([col] (edn-deserialize-callback col nil {}))
+  ([col default-value] (edn-deserialize-callback col default-value {}))
+  ([col default-value reader-opts]
    (assert (keyword? col))
    (fn [r]
      (let [col? (contains? r col)
            v (get r col)]
+       ;;:completion_status [:started] nil
        (cond
          ;; i would prefer not to set the value if the key
          ;; wasn't in the map, but lots of test breakage results
          ;; (not col?) r
          (nil? v) (assoc r col default-value)
          (not (string? v)) r
-         :else (update r col edn/read-string))))))
+         :else (update r col #(edn/read-string reader-opts %)))))))
