f7023b69b6c61fe9e40236514f83f9edc556d4b6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2018-10-18 12:55:41 +0100

    reinstate NULL semantics for edn_codec

--- src/er_cassandra/model/callbacks/edn_codec.clj
+++ src/er_cassandra/model/callbacks/edn_codec.clj
@@ -11,7 +11,12 @@
       (cond
         ;; i would prefer not to set the value if the key
         ;; wasn't in the map, but it breaks a lot of tests
-        ;; (not col?) r
+        ;;
+        ;; UPDATE 20181018 mccraig - we gotta deal with the
+        ;; test breakage 'cos this is causing :attrs and
+        ;; :perms to get temporarily removed from :org_user
+        ;; records during import with consequent perms failures
+        (not col?) r
         (nil? v) (assoc r col nil)
         (and (string? v) (empty? v)) (assoc r col nil)
         (string? v) r
@@ -29,7 +34,12 @@
        (cond
          ;; i would prefer not to set the value if the key
          ;; wasn't in the map, but lots of test breakage results
-         ;; (not col?) r
+         ;;
+         ;; UPDATE 20181018 mccraig - we gotta deal with the
+         ;; test breakage 'cos this is causing :attrs and
+         ;; :perms to get temporarily removed from :org_user
+         ;; records during import with consequent perms failures
+         (not col?) r
          (nil? v) (assoc r col default-value)
          (not (string? v)) r
          :else (update r col #(edn/read-string reader-opts %)))))))
