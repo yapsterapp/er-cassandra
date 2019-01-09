02ebb3b1ff95b652deeb22a8e1d72581f12516e9
Adam Harper <github@adam-harper.com>
2019-01-09 20:41:02 +0800

    cassandra: add support for specifying cluster ordering in migration schema helpers

--- src/er_cassandra/migrations/schema_helpers.clj
+++ src/er_cassandra/migrations/schema_helpers.clj
@@ -14,6 +14,34 @@
       (string/replace-first \,\()
       (str \))))
 
+(def clustering-order-directions
+  #{:asc :desc})
+
+(defn clustering-order-entry?
+  [x]
+  (if (sequential? x)
+    (and (= 2 (count x)) (some? (clustering-order-directions (last x))))
+    true))
+
+(defn clustering-order-entry-str
+  [e]
+  (if (sequential? e)
+    (let [[col-name dir] e]
+      (str (name col-name) " " (string/upper-case (name dir))))
+    (str (name e) " ASC")))
+
+(defn clustering-order-str
+  [clustering-order]
+  (prn "co" clustering-order)
+  (-> (reduce
+       (fn [rs cg]
+         (if (clustering-order-entry? cg)
+           (str rs ", " (clustering-order-entry-str cg))
+           (str rs ", " (clustering-order-str cg))))
+       ""
+       clustering-order)
+      (string/replace-first #", " "")))
+
 (defn drop-view
   [{v-name :name}]
   (string/join " " ["drop materialized view if exists" (name v-name)]))
@@ -35,11 +63,22 @@
      #(contains? column-names %)
      (flatten primary-key))))
 
+(defn primary-key-contains-clustering-order-cols?
+  [primary-key clustering-order]
+  (let [primary-key-cols (set (flatten primary-key))
+        clustering-order-cols (->> clustering-order
+                                   flatten
+                                   (remove #(clustering-order-directions %)))]
+    (every?
+     #(contains? primary-key-cols %)
+     clustering-order-cols)))
+
 (defn create-table
   [{v-name :name
     v-primary-key :primary-key
     v-columns :columns
     v-compaction :compaction
+    v-clustering-order :clustering-order
     :as table-definition}]
   (assert (every? some? [v-name v-primary-key v-columns])
           (str "invalid table definition"
@@ -47,6 +86,10 @@
                (pr-str table-definition)))
   (assert (all-primary-key-columns-exist? (map first v-columns) v-primary-key)
           (str "not all primary key columns are defined!"
+               "\n\n"
+               (pr-str table-definition)))
+  (assert (primary-key-contains-clustering-order-cols? v-primary-key v-clustering-order)
+          (str "not all cluster ordering columns are in the primary key!"
                "\n\n"
                (pr-str table-definition)))
   (assert (or (nil? v-compaction) (#{:leveled :size-tiered} v-compaction)))
@@ -62,6 +105,8 @@
         v-columns
         primary-key-str))
       ")"
+      (when (some? v-clustering-order)
+        (str "WITH CLUSTERING ORDER BY (" (clustering-order-str v-clustering-order) ")"))
       (case v-compaction
         nil nil
         :leveled (str "WITH " leveled-compaction-clause)
@@ -73,6 +118,7 @@
     v-primary-key :primary-key
     v-selected-columns :selected-columns
     v-compaction :compaction
+    v-clustering-order :clustering-order
     :as view-definition}]
   (assert (every? some? [v-name v-from v-primary-key v-selected-columns])
           (str "invalid view definition"
@@ -80,6 +126,10 @@
                (pr-str view-definition)))
   (assert (all-primary-key-columns-exist? v-selected-columns v-primary-key)
           (str "views must include all primary key columns"
+               "\n\n"
+               (pr-str view-definition)))
+  (assert (primary-key-contains-clustering-order-cols? v-primary-key v-clustering-order)
+          (str "not all cluster ordering columns are in the primary key!"
                "\n\n"
                (pr-str view-definition)))
   (assert (or (nil? v-compaction) (#{:leveled :size-tiered} v-compaction)))
@@ -99,6 +149,8 @@
       "from" v-from
       "where" where-clauses
       "primary key" primary-key-str
+      (when (some? v-clustering-order)
+        (str "WITH CLUSTERING ORDER BY (" (clustering-order-str v-clustering-order) ")"))
       (case v-compaction
         nil nil
         :leveled (str "WITH " leveled-compaction-clause)
